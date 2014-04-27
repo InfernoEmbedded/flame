8f85ff53a5a4785f59b7349f7b19ac0bac69f778
Alastair D'Silva <alastair@d-silva.org>
2014-04-27 15:05:39 +1000

    Move helper functions to Helpers.cpp

--- flame/Helpers.cpp
+++ flame/Helpers.cpp
@@ -3,7 +3,17 @@
  * it won't get linked unless there's a reference to it needed.
  */
 #include <avr/interrupt.h>
+#include <stdlib.h>
+
 extern "C" __attribute__ ((noreturn)) void __cxa_pure_virtual() {
 	cli();
 	for (;;);
+}
+
+void* operator new(size_t objsize) {
+	return malloc(objsize);
+}
+
+void operator delete(void* obj) {
+	free(obj);
 }
